3a80a73a77b2f25102249e2b841a71b966918106
“Gigi <debortolg@gmail.com>
2022-10-17 00:47:19 -0700

    added questions and began working on functions - forgot to make separate branches to do so - will keep in mind for further pushes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Inquirer Variable
 const inquirer = require('inquirer');
+// Importing exports from the generateMarkdown file
 const generateMarkdown = require('./utils/generateMarkdown');
+// File Systems
 const fs = require('fs');
 
-// Array of questions for user input
+// Questions
 const questions = [
         {
             type: 'input',
@@ -16,22 +19,55 @@ const questions = [
         },
         {
             type: 'input',
-            name: 'motive',
-            message: ''
+            name: 'installation',
+            message: 'Please provide instructions on how to run and install your project.'
         }, 
         {
             type: 'input',
-            name: 'motive',
-            message: ''
+            name: 'contributing',
+            message:''
+        }, 
+        {
+            type: 'input',
+            name: 'tests',
+            message:''
+        }, 
+        {
+            type: 'input',
+            name: 'Credits',
+            message:''
+        }, 
+        {
+            type: 'checkbox',
+            name: 'license',
+            message:'What license type did you use for this project?',
+            choices: ['Apache', 'BSD', 'GNU','MIT', 'Mozilla', 'ISC', 'Other']
+        }, 
+        {
+            type: 'input',
+            name: 'questions',
+            message:'Please enter your GitHub username'
+        }, 
+        {
+            type: 'input',
+            name: 'questions',
+            message:'Please enter your email address'
         }
     ]
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile('./${fileName}.md', generateMarkdown(data), err => {
+        if (err) throw err;
+        console.log('The file has been Created!');
+    });
+ }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
